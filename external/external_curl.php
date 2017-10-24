f7328cfb24234be22042345b7f117db8835ad4da
Marlito Dungog <marlito.dungog@gmail.com>
2017-10-23 18:28:36 -0700

    curl php

---
+++ external/external_curl.php
@@ -0,0 +1,29 @@
+<?php 
+$json_return = array(
+	'is_success' => false
+);
+$data = $_POST;
+
+$post_fields = array();
+foreach( $_POST as $key => $value ){
+	$post_fields[$key] = $value
+}
+
+$ch  = curl_init();
+$url = 'http://holisticwebpresencecrm.com/external_requests/ajax_register_leads';
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$post_fields);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+curl_close($ch);
+
+
+if ($server_output == "OK") { 
+	$json_return['is_success'] = true;
+}
+
+echo json_encode($json_return);
