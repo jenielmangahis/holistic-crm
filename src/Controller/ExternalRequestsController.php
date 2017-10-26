a6c8cb7085759987c3f03050238db2a0dd3eb41f
Bryann <bryann.revina@gmail.com>
2017-10-26 17:32:34 +0800

    Update ExternalRequestsController.php got arctic website   - 10-26-2017

--- src/Controller/ExternalRequestsController.php
+++ src/Controller/ExternalRequestsController.php
@@ -37,6 +37,104 @@ class ExternalRequestsController extends AppController
       $this->AllocationUsers = TableRegistry::get('AllocationUsers');
 
       $data = $this->request->query;
+      
+      $json['is_success'] = false;      
+
+      if( $data ){
+        $lead = $this->Leads->newEntity();
+
+        $lead_action = "";
+        if( isset($data['lead-action']) ){
+          $lead_action = $data['lead-action'];
+        }
+
+        $source_url = "";
+        if( isset($data['lead-url']) ){
+          $source_url = $data['lead-url'];
+        }
+
+        $data_leads = [
+          'firstname' => $data['lead-firstname'],
+          'surname' => $data['lead-lastname'],
+          'email' => $data['lead-email'],
+          'phone' => $data['lead-phone'],
+          'city' => $data['lead-city'],
+          'state' => $data['lead-state'],
+          'source_id' => $data['lead-source-id'],
+          'lead_action' => $lead_action,
+          'status_id' => 2,
+          'lead_type_id' => 1,
+          'source_url' => $source_url,
+          'interest_type_id' => 6,
+          'allocation_id' => $data['lead-allocation-id'],
+          'allocation_date' => date("Y-m-d"),
+          'followup_date' => date("Y-m-d"),
+          'followup_action_reminder_date' => date("Y-m-d")
+        ];
+        $lead = $this->Leads->patchEntity($lead, $data_leads);        
+        if ($new_lead = $this->Leads->save($lead)) {
+
+            $allocation_users = $this->AllocationUsers->find('all')
+                ->contain(['Users'])
+                ->where(['AllocationUsers.allocation_id' => $data['lead-allocation-id']])
+            ;
+
+            $users_email = array();
+            foreach($allocation_users as $users){            
+                $users_email[$users->user->email] = $users->user->email;            
+            }    
+
+            //add other emails to be sent - start
+              foreach($allocation_users as $users){            
+                  $other_email_to_explode = $users->user->other_email;
+
+                  if( !empty($other_email_to_explode) || $other_email_to_explode != '' ) {
+
+                    $other_email = explode(";", $other_email_to_explode);
+
+                    foreach($other_email as $oekey => $em) {
+
+                      if (trim($em) != '') {
+                          $other_email_to_add = $em; //ltrim($em);
+                          $users_email[$other_email_to_add] = $other_email_to_add;  
+                      }
+
+                    }
+                    
+                  }
+              }    
+            //add other emails to be sent - end                   
+
+            if( !empty($users_email) ){
+              //Send email notification
+              $leadData = $this->Leads->get($new_lead->id, [
+                  'contain' => ['Statuses', 'Sources', 'Allocations', 'LeadTypes','InterestTypes']
+              ]);  
+              $email_customer = new Email('default'); //default or cake_smtp (for testing in local)
+              $email_customer->from(['websystem@holisticwebpresencecrm.com' => 'Holistic'])
+                ->template('external_leads_registration')
+                ->emailFormat('html')          
+                ->bcc($users_email)                                                                                               
+                ->subject('New Lead')
+                ->viewVars(['new_lead' => $leadData->toArray()])
+                ->send();
+            }
+            
+          $json['is_success'] = true;
+        } 
+      }
+
+      $this->viewBuilder()->layout('');     
+      echo json_encode($json);
+      exit;
+    } 
+
+    public function ajax_register_leads_arctic()
+    {
+      $this->AllocationUsers = TableRegistry::get('AllocationUsers');
+
+      $data = $this->request->query;
+
       $json['is_success'] = false;      
 
       if( $data ){
