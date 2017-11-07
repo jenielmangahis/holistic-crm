aa990de4e37cd3c6a9aa9a117102b1f91ed160d9
Bryann Revina <bryann.revina@gmail.com>
2017-11-07 17:18:52 +0800

    ...

--- external/external_curl.php
+++ external/external_curl.php
@@ -21,6 +21,7 @@ $server_output = curl_exec ($ch);
 
 curl_close($ch);
 
+/*
 $ch  = curl_init();
 $url = 'http://holisticwebpresencecrm.com/beta/beta_external_requests/ajax_post_register_leads';
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -32,7 +33,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $server_output = curl_exec ($ch);
 
 curl_close($ch);
-
+*/
 
 if ($server_output == "OK") { 
 	$json_return['is_success'] = true;
