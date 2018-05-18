6600bfeec84df9be872b9f877dd7ecd6a8d23239
Bryan Bio <works.bryan.bio@gmail.com>
2018-05-18 14:41:28 +0700

    Added libraries to crm Created sample function, for testing, phpexcel chart http://prntscr.com/jjdytw

--- src/Controller/ReportsController.php
+++ src/Controller/ReportsController.php
@@ -1034,7 +1034,100 @@ class ReportsController extends AppController
             break;
         }        
         if( $report_data['s3']['report-type'] == 'Excel' ){
-
+          define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+          $objPHPExcel = new PHPExcel();
+          $objWorksheet = $objPHPExcel->getActiveSheet();
+          $objWorksheet->fromArray(
+            array(
+              array('',2010,2011,2012,2013,2014),
+              array('Q1',12,15,21,2,5),
+              array('Q2',56,73,86,10,6),
+              array('Q3',52,61,69,22,11),
+              array('Q4',30,32,0,2,25),
+              array('Q5',30,32,0,2,25),
+            )
+          );
+          //  Set the Labels for each data series we want to plot
+          //    Datatype
+          //    Cell reference for data
+          //    Format Code
+          //    Number of datapoints in series
+          //    Data values
+          //    Data Marker
+          $dataseriesLabels = array(
+            new \PHPExcel_Chart_DataSeriesValues('String', 'Worksheet!$B$1', null, 1), //  2010
+            new \PHPExcel_Chart_DataSeriesValues('String', 'Worksheet!$C$1', null, 1), //  2011
+            new \PHPExcel_Chart_DataSeriesValues('String', 'Worksheet!$D$1', null, 1), //  2012
+            new \PHPExcel_Chart_DataSeriesValues('String', 'Worksheet!$E$1', null, 1), //  2013
+            new \PHPExcel_Chart_DataSeriesValues('String', 'Worksheet!$F$1', null, 1), //  2014
+          );
+          //  Set the X-Axis Labels
+          //    Datatype
+          //    Cell reference for data
+          //    Format Code
+          //    Number of datapoints in series
+          //    Data values
+          //    Data Marker
+          $xAxisTickValues = array(
+            new \PHPExcel_Chart_DataSeriesValues('String', 'Worksheet!$A$2:$A$6', null, 5),  //  Q1 to Q4
+          );
+          //  Set the Data values for each data series we want to plot
+          //    Datatype
+          //    Cell reference for data
+          //    Format Code
+          //    Number of datapoints in series
+          //    Data values
+          //    Data Marker
+          $dataSeriesValues = array(
+            new \PHPExcel_Chart_DataSeriesValues('Number', 'Worksheet!$B$2:$B$6', null, 5),
+            new \PHPExcel_Chart_DataSeriesValues('Number', 'Worksheet!$C$2:$C$6', null, 5),
+            new \PHPExcel_Chart_DataSeriesValues('Number', 'Worksheet!$D$2:$D$6', null, 5),
+            new \PHPExcel_Chart_DataSeriesValues('Number', 'Worksheet!$E$2:$E$6', null, 5),
+            new \PHPExcel_Chart_DataSeriesValues('Number', 'Worksheet!$F$2:$F$6', null, 5),
+          );
+          //  Build the dataseries
+          $series = new \PHPExcel_Chart_DataSeries(
+            \PHPExcel_Chart_DataSeries::TYPE_LINECHART,    // plotType
+            \PHPExcel_Chart_DataSeries::GROUPING_STACKED,  // plotGrouping
+            range(0, count($dataSeriesValues)-1),     // plotOrder
+            $dataseriesLabels,                // plotLabel
+            $xAxisTickValues,               // plotCategory
+            $dataSeriesValues               // plotValues
+          );
+          //  Set the series in the plot area
+          $plotarea = new \PHPExcel_Chart_PlotArea(null, array($series));
+          //  Set the chart legend
+          $legend = new \PHPExcel_Chart_Legend(\PHPExcel_Chart_Legend::POSITION_TOPRIGHT, null, false);
+          $title = new \PHPExcel_Chart_Title('Test Stacked Line Chart');
+          $yAxisLabel = new \PHPExcel_Chart_Title('Value ($k)');
+          //  Create the chart
+          $chart = new \PHPExcel_Chart(
+            'chart1',   // name
+            $title,     // title
+            $legend,    // legend
+            $plotarea,    // plotArea
+            true,     // plotVisibleOnly
+            0,        // displayBlanksAs
+            null,     // xAxisLabel
+            $yAxisLabel   // yAxisLabel
+          );
+          //  Set the position where the chart should appear in the worksheet
+          $chart->setTopLeftPosition('A7');
+          $chart->setBottomRightPosition('H20');
+          //  Add the chart to the worksheet
+          $objWorksheet->addChart($chart);
+          // Save Excel 2007 file
+          echo date('H:i:s') , " Write to Excel2007 format" , EOL;
+          $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+          $objWriter->setIncludeCharts(TRUE);
+          $objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+          echo date('H:i:s') , " File written to " , str_replace('.php', '.xlsx', pathinfo(__FILE__, PATHINFO_BASENAME)) , EOL;
+          // Echo memory peak usage
+          echo date('H:i:s') , " Peak memory usage: " , (memory_get_peak_usage(true) / 1024 / 1024) , " MB" , EOL;
+          // Echo done
+          echo date('H:i:s') , " Done writing file" , EOL;
+          echo 'File has been created in ' , getcwd() , EOL;
+          exit;
         }else{          
           $this->set([
             'chart_labels' => $chart_labels,
@@ -1046,7 +1139,9 @@ class ReportsController extends AppController
         $this->Flash->error(__('Cannot generate report'));
         return $this->redirect(['action' => 'cumulative_step3']);
       }
+    }
 
-
+    public function generate_cumulative_chart_excel() {
+      
     }
 }
